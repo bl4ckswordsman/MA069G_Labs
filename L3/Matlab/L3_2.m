5f65daba01daab193f9aefad32c499702425fe95
Amarildo Rajta <amra2101@student.miun.se>
2023-09-18 17:25:42 +0200

    L3 update

--- L3/Matlab/L3_2.m
+++ L3/Matlab/L3_2.m
@@ -1,4 +1,36 @@
 %L3_2
+clc
+clear
 
-%Wolfram Alpha:
-% solve(r == {{0.8, 0.15, 0.05},{0.75, 0.10, 0.15},{0.5, 0.25, 0.25]}}*r and sum(r)==1)
+P = [0.8, 0.15, 0.05;
+    0.75, 0.10, 0.15;
+    0.5, 0.25, 0.25];
+
+%1. Wolfram Alpha:
+% P={{0.8,0.15,0.05},{0.75,0.10,0.15},{0.5, 0.25, 0.25}}, x = {0.5, 0.25, 0.25}
+%solve x1+x2+x3=1,
+%x1=0.8x1+0.75x2+0.5x3,
+%x2=0.15x1+0.1x2+0.25x3,
+%x3=0.05x1+0.15x2+0.25*x3
+
+%1. eig funkt. för egenvärde och egenvekt.
+%[V, D] = eig(P');
+%stationar_losning = V(:,1) / sum(V(:,1));
+
+%2. Vänster matrisdivision
+%A = P' - eye(size(P));
+%b = zeros(size(P, 1), 1);
+%stationar_losning = A \ b;
+%stationar_losning = stationar_losning / sum(stationar_losning);
+
+%3. Symboliska beräkningar:
+syms x1 x2 x3;
+eq1 = x1 * P(1,1) + x2 * P(2,1) + x3 * P(3,1) == x1;
+eq2 = x1 * P(1,2) + x2 * P(2,2) + x3 * P(3,2) == x2;
+eq3 = x1 * P(1,3) + x2 * P(2,3) + x3 * P(3,3) == x3;
+eq4 = x1 + x2 + x3 == 1;
+
+losning = solve([eq1, eq2, eq3, eq4], [x1, x2, x3]);
+stationar_losning = [losning.x1, losning.x2, losning.x3];
+
+disp(vpa(stationar_losning,6));
---
+++ L3/Matlab/L3_3.m
@@ -0,0 +1,28 @@
+%L3_3
+clc
+clear
+
+P = [0.8, 0.15, 0.05;
+    0.75, 0.10, 0.15;
+    0.5, 0.25, 0.25];
+
+it_count = 1000;
+f = [0, 0, 0];
+k = randi([1,3]);
+
+for i = 1:it_count
+    % Beräkna nästa tillstånd baserat på nuvarande tillstånd k
+    next_state = randsample(1:3, 1, true, P(k,:));
+    
+    % Uppdatera frekvensen för nästa tillstånd
+    f(next_state) = f(next_state) + 1;
+    
+    % Uppdatera nuvarande tillstånd k
+    k = next_state;
+end
+
+% Beräkna de relativa frekvenserna
+rel_freq = f / sum(f);
+
+disp('Stationära sannolikheter efter Monte Carlo-simulering:');
+disp(rel_freq);

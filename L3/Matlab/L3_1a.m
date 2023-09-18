1fe52e8e57028da0744fe6c2cefae94720b067fa
Amarildo Rajta <amra2101@student.miun.se>
2023-09-18 16:19:03 +0200

    L3 update

--- L3/Matlab/L3_1a.m
+++ L3/Matlab/L3_1a.m
@@ -7,23 +7,27 @@ P = [0.8, 0.15, 0.05;
     0.5, 0.25, 0.25];
 
 %L3_1b
-v0 = [0.2 0.3 0.5];
+v0 = [0.1 0.1 0.2];
 tol = 1e-6;
+v = v0;
 
 err = inf;
 it = 0;
 while (err > tol)
-    v_new = v0*P;
+    v_new = v*P;
+    % Ett-normen
+    err = norm(v_new - v, 1);
     % Två-normen
-    err = norm(v_new - v0);
-    v0 = v_new;
+    %err = norm(v_new - v, 2);
+    v = v_new;
     it = it + 1;
 end
 
-disp(v0);
+disp('Stationär lösning:');
+disp(v);
 %L3_1c
 disp(['Toleransnivå: ', num2str(tol)]);
 disp(['Antal iterationer: ', num2str(it)]);
 
-%Startvektorn har ingen betyd. för stat.result -> ergodisk
-
+%Startvektorn har betyd. för stat.result -> ergodisk
+%Startvekt. påverkar konvergenstiden

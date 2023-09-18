74ee020e2130f112e67295057dba11ded36b15e1
Amarildo Rajta <amra2101@student.miun.se>
2023-09-18 19:22:46 +0200

    Create L3_4a.m

---
+++ L3/Matlab/L3_4a.m
@@ -0,0 +1,29 @@
+%L3_4a
+clc
+clear
+
+P = [0 1 0; 0 0 1; 1 0 0];
+v0 = [0.3 0.7 0.2];
+tol = 1e-5;
+v = v0;
+
+err = inf;
+it = 0;
+while (err > tol)
+    v_new = v*P;
+    % Ett-normen
+    err = norm(v_new - v, 1);
+    % Två-normen
+    %err = norm(v_new - v, 2);
+    v = v_new;
+    it = it + 1;
+    if (it > 1e6)
+        break;
+    end
+end
+
+disp('Stationär lösning:');
+disp(v);
+
+disp(['Toleransnivå: ', num2str(tol)]);
+disp(['Antal iterationer: ', num2str(it)]);

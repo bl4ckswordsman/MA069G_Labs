14cac7cd8d8de497ad0e4e552cf1444ceee7dc3c
Amarildo Rajta <amra2101@student.miun.se>
2023-09-27 01:26:45 +0200

    Update L4_1.m

--- L4/Matlab/L4_1.m
+++ L4/Matlab/L4_1.m
@@ -2,11 +2,18 @@
 clc
 clear
 
+hold on
+axis([0 115 0 65])
+x1 = [0:1:115];
 % Investment constraint:
 % 110.5x + 198.5y <= 12000
+y1 = -291/397*x1 + 24000/397;
 
 % Labour constraint:
 % 11x + 32y <= 1250
+y2 = -11/32*x1 + 625/16;
 
 % Objective function:
 % Z = max(51.5x + 119.5y)
+
+plot(x1, y1, 'r', x1, y2, 'b')

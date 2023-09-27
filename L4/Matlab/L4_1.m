cbe4983b44b8cacf830212c70200c71c88cb067e
Amarildo Rajta <amra2101@student.miun.se>
2023-09-27 17:13:25 +0200

    Update L4_1.m

--- L4/Matlab/L4_1.m
+++ L4/Matlab/L4_1.m
@@ -3,17 +3,39 @@ clc
 clear
 
 hold on
-axis([0 115 0 65])
-x1 = [0:1:115];
+axis([95 120 0 100])
+x1 = [95:0.1:120];
+
+% Area constraint:
+% x + y <= 110
+y1 = -x1 + 110;
+
 % Investment constraint:
 % 110.5x + 198.5y <= 12000
-y1 = -291/397*x1 + 24000/397;
+y2 = -221/397*x1 + 24000/397;
 
 % Labour constraint:
 % 11x + 32y <= 1250
-y2 = -11/32*x1 + 625/16;
+y3 = -11/32*x1 + 625/16;
+
+% Non-negative constraint:
+% x, y >= 0
 
 % Objective function:
 % Z = max(51.5x + 119.5y)
 
-plot(x1, y1, 'r', x1, y2, 'b')
+% Define the feasible region
+[X,Y] = meshgrid(x1,0:0.01:15);
+cond1 = X+Y<=110;
+cond2 = 110.5*X+198.5*Y<=12000;
+cond3 = 11*X+32*Y<=1250;
+cond4 = X>=0;
+cond5 = Y>=0;
+Z = cond1 & cond2 & cond3 & cond4 & cond5;
+
+% Plot the feasible region
+imagesc(x1, [0 15], Z);
+colormap(flipud(gray));
+
+plot(x1, y1, 'g', x1, y2, 'r', x1, y3, 'b')
+legend('x+y <= 110', '110.5x + 198.5y <= 12000', '11x + 32y <= 1250');
